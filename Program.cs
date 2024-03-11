68cc6390b148294141ae42a19001a8d6b572ad69
Fabiolll <fabio95.df@gmail.com>
2024-03-11 06:42:13 -0300

    criacao do projeto

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace NotaAlunoEstruturado;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double notaF=0;
+        double[] nota = new double[3];
+        Console.WriteLine("Calcular nota aluno");
+        for (int i = 0; i < 3; i++)
+        {
+            Console.WriteLine("Informe a " + (i + 1) + "ª nota: ");
+            nota[i] = double.Parse(Console.ReadLine());
+            notaF = notaF + nota[i];
+        }
+        notaF = nota[0] + nota[1] + nota[2];
+        Console.WriteLine("nota final do aluno: " + notaF);
+        if(notaF >= 60 )
+        Console.WriteLine("APROVADO!");
+        else
+        Console.WriteLine("REPROVADO!\n" +"Faltam "+(60-notaF)+" pontos para o aluno ser aprovado.");
+    }
+}
